84464a1a32bb7f5e2cf2b83106247f9dea5f7295
Christian Engler <christian.engler@despegar.com>
2021-08-26 19:27:36 -0300

    game header and footer

---
+++ src/components/GameHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="wrapperHeader">
+    <div class="header" :style="{ backgroundImage: 'url(' + image + ') repeat-x 0 0' }">
+    </div> 
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.wrapperHeader {
+  padding-bottom: 30px;
+}
+
+.wrapperHeader .header {
+  height:170px;
+  margin:0 auto;
+  width: 100%;
+  background-position: center; 
+  text-align: center;
+}
+</style>
