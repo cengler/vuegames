873ec4f8b85ed4f5cea156fe3d8a3cdc965aa2b1
Christian Engler <christian.engler@despegar.com>
2021-08-26 19:20:16 -0300

    footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    <p>boreal games</p>
+    <template v-for="l in licences" :key="l.component">
+      <p><a :href="l.url">{{l.component}} by {{l.author}}</a></p>
+    </template>
+  </footer>
+</template>
+
+<script>
+export default {
+  props: {
+    licences: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
